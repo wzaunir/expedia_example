7056d3402e181f56bae20df453eff62aecd32aa7
wzaunir <wzaunir@gmail.com>
2025-08-06 15:21:48 -0300

    Add property content retrieval

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,5 @@ use App\Http\Middleware\ApiTokenMiddleware;
 Route::middleware([ApiTokenMiddleware::class])->group(function () {
     Route::get('/expedia/hotels', [ExpediaController::class, 'searchHotels']);
     Route::get('/expedia/region/{region_id}', [ExpediaController::class, 'getRegion']);
+    Route::get('/expedia/property-content', [ExpediaController::class, 'getPropertyContent']);
 });
