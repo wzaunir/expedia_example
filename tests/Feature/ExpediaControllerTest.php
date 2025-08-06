dbb5180fa7d7e1ce105dff40c68762609ec9dba6
wzaunir <wzaunir@gmail.com>
2025-08-06 15:16:22 -0300

    Add success and error tests for ExpediaController

--- tests/Feature/ExpediaControllerTest.php
+++ tests/Feature/ExpediaControllerTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 
 class ExpediaControllerTest extends TestCase
 {
-    public function test_search_hotels_returns_response()
+    public function test_search_hotels_success()
     {
         Http::fake([
             'https://test.expediapartnercentral.com/rapid/hotels*' => Http::response([
@@ -27,7 +27,34 @@ class ExpediaControllerTest extends TestCase
         $middleware = new ApiTokenMiddleware();
         $response = $middleware->handle($request, fn($req) => $controller->searchHotels($req));
 
+        Http::assertSent(function ($request) {
+            return $request->hasHeader('Authorization', 'Bearer demo-key');
+        });
+
         $this->assertEquals(200, $response->status());
         $this->assertNotEmpty($response->getData(true)['hotels']);
+    }
+
+    public function test_search_hotels_with_invalid_city_returns_error()
+    {
+        Http::fake([
+            'https://test.expediapartnercentral.com/rapid/hotels*' => Http::response([
+                'message' => 'Invalid cityId',
+            ], 422)
+        ]);
+
+        $request = Request::create('/api/expedia/hotels', 'GET', ['cityId' => 'invalid']);
+        $request->headers->set('X-API-TOKEN', 'secret-token');
+
+        $controller = new ExpediaController();
+        $middleware = new ApiTokenMiddleware();
+        $response = $middleware->handle($request, fn($req) => $controller->searchHotels($req));
+
+        Http::assertSent(function ($request) {
+            return $request->hasHeader('Authorization', 'Bearer demo-key');
+        });
+
+        $this->assertEquals(422, $response->status());
+        $this->assertEquals('Invalid cityId', $response->getData(true)['message']);
     }
 }
