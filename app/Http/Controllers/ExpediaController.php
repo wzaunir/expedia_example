0746e3cf74dfb9ca5aa989c839ba2a70e61cfd3b
wzaunir <wzaunir@gmail.com>
2025-08-06 15:15:05 -0300

    Validate Expedia query parameters

--- app/Http/Controllers/ExpediaController.php
+++ app/Http/Controllers/ExpediaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Validator;
 
 class ExpediaController extends Controller
 {
@@ -12,13 +13,23 @@ class ExpediaController extends Controller
      */
     public function searchHotels(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'cityId' => 'required|integer',
+            'checkin' => 'required|date_format:Y-m-d',
+            'checkout' => 'required|date_format:Y-m-d|after:checkin',
+            'room1' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $params = $validator->validated();
+
         $response = Http::withHeaders([
             'Accept' => 'application/json',
             'Authorization' => 'Bearer ' . config('services.expedia.key'),
-        ])->get('https://test.expediapartnercentral.com/rapid/hotels', [
-            'cityId' => $request->query('cityId', '1506246'),
-            'room1' => '2',
-        ]);
+        ])->get('https://test.expediapartnercentral.com/rapid/hotels', $params);
 
         return response()->json($response->json(), $response->status());
     }
