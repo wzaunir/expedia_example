8081a02f15fbf276febb4f48fe21868becef1de0
wzaunir <wzaunir@gmail.com>
2025-08-06 15:08:38 -0300

    Handle Expedia API errors

--- app/Http/Controllers/ExpediaController.php
+++ app/Http/Controllers/ExpediaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class ExpediaController extends Controller
 {
@@ -12,14 +13,34 @@ class ExpediaController extends Controller
      */
     public function searchHotels(Request $request)
     {
-        $response = Http::withHeaders([
-            'Accept' => 'application/json',
-            'Authorization' => 'Bearer ' . config('services.expedia.key'),
-        ])->get('https://test.expediapartnercentral.com/rapid/hotels', [
-            'cityId' => $request->query('cityId', '1506246'),
-            'room1' => '2',
-        ]);
+        try {
+            $response = Http::withHeaders([
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer ' . config('services.expedia.key'),
+            ])->get('https://test.expediapartnercentral.com/rapid/hotels', [
+                'cityId' => $request->query('cityId', '1506246'),
+                'room1' => '2',
+            ]);
 
-        return response()->json($response->json(), $response->status());
+            if ($response->successful()) {
+                return response()->json($response->json(), $response->status());
+            }
+
+            Log::error('Expedia API returned error', [
+                'status' => $response->status(),
+                'body' => $response->body(),
+            ]);
+
+            return response()->json([
+                'message' => 'Expedia API error',
+                'status' => $response->status(),
+            ], $response->status());
+        } catch (\Throwable $e) {
+            Log::error('Expedia API request failed', ['exception' => $e->getMessage()]);
+            return response()->json([
+                'message' => 'Expedia API request failed',
+                'status' => 500,
+            ], 500);
+        }
     }
 }
--- tests/Feature/ExpediaControllerTest.php
+++ tests/Feature/ExpediaControllerTest.php
@@ -30,4 +30,42 @@ class ExpediaControllerTest extends TestCase
         $this->assertEquals(200, $response->status());
         $this->assertNotEmpty($response->getData(true)['hotels']);
     }
+
+    public function test_search_hotels_handles_api_error()
+    {
+        Http::fake([
+            'https://test.expediapartnercentral.com/rapid/hotels*' => Http::response([], 500),
+        ]);
+
+        $request = Request::create('/api/expedia/hotels', 'GET', ['cityId' => '1506246']);
+        $request->headers->set('X-API-TOKEN', 'secret-token');
+
+        $controller = new ExpediaController();
+        $middleware = new ApiTokenMiddleware();
+        $response = $middleware->handle($request, fn($req) => $controller->searchHotels($req));
+
+        $this->assertEquals(500, $response->status());
+        $data = $response->getData(true);
+        $this->assertEquals(500, $data['status']);
+        $this->assertArrayHasKey('message', $data);
+    }
+
+    public function test_search_hotels_handles_network_exception()
+    {
+        Http::fake(function ($request) {
+            throw new \Exception('Network error');
+        });
+
+        $request = Request::create('/api/expedia/hotels', 'GET', ['cityId' => '1506246']);
+        $request->headers->set('X-API-TOKEN', 'secret-token');
+
+        $controller = new ExpediaController();
+        $middleware = new ApiTokenMiddleware();
+        $response = $middleware->handle($request, fn($req) => $controller->searchHotels($req));
+
+        $this->assertEquals(500, $response->status());
+        $data = $response->getData(true);
+        $this->assertEquals(500, $data['status']);
+        $this->assertArrayHasKey('message', $data);
+    }
 }
