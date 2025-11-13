95cf3814cdbb7a26576b6a5888001f5b160791e3
wzaunir <wzaunir@gmail.com>
2025-11-13 19:26:07 +0000

    correçao home

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json(['status' => 'ok']);
+    return view('welcome');   // se você tiver o view padrão habilitado
 });
